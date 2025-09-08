109fed70ac967d3c0d8f42dd3ba9caa2c2931578
Tran Minh Tien <tmt31032005@gmail.com>
2025-09-08 22:29:18 +0700

    update database setup

---
+++ backend/models/story.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from db.database import Base
+
+class Story():
+  pass
